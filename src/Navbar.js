c48c75c2c565708be85481afb6692048b54dd999
Jainam Panchal <66761580+jainam-panchal@users.noreply.github.com>
2023-06-21 13:35:36 +0530

    Navbar Bug Patched

--- src/Navbar.js
+++ src/Navbar.js
@@ -14,7 +14,7 @@ function Navbar() {
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav ms-auto">
                             <li className="nav-item">
-                                <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
+                                <NavLink className="nav-link" exact aria-current="page" to="/">Home</NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink className="nav-link" to="/about">About</NavLink>
