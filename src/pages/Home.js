e7785ac40ccc60fa233a3d2b5d883456b48a65dc
Jainam Panchal <66761580+jainam-panchal@users.noreply.github.com>
2023-06-22 16:32:19 +0530

    Setup Node API with AXIOS

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,18 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 const Home = () => {
+    const [users,setUsers] = useState([]);
+    
+    useEffect(()=>{
+        loadUsers();
+    },[])
+
+    const loadUsers = async () => {
+        const result =  await axios.get("http://localhost:3001/users");
+        setUsers(result.data);
+    }
+
     return(
         <div className="p-5">
             <h1>Home Page</h1>
